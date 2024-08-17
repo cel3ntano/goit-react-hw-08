f7fa0ec52d7c32bc4da2e45088b24d4d46f6e420
cel3ntano <adriano2024celentano@gmail.com>
2024-08-17 23:15:20 +0200

    configured persistor

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import App from "./components/App.jsx";
 import "modern-normalize";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>
 );
